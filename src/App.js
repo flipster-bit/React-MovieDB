33bec4403f3a2e8f33baeb448afbbde835feb537
Himanshu Notani <hnotani7@gmail.com>
2021-11-23 15:37:35 +0530

    Update App.js
    updating Key

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=2ab8e5a7`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
