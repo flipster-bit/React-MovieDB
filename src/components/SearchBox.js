977812b47a554f561cc820c1ee828fcf3a301e31
Himanshu Notani <hnotani7@gmail.com>
2022-02-25 10:39:15 +0530

    Update SearchBox.js
    Adding debounce feature

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 const SearchBox = (props) => {
+	const debouncedSave = useRef(debounce(nextVal => props.setSearchValue(nextVal), 1000))
+		.current;
+	
+	const handleSearch = (event) =>{
+
+		const nextVal = event.target.value;
+		//props.setSearchValue(nextVal);
+		
+		debouncedSave(nextVal);
+	}
 	return (
 		<div className='col col-sm-4'>
 			<input
 				className='form-control'
 				value={props.value}
-				onChange={(event) => props.setSearchValue(event.target.value)}
+				onChange={handleSearch}
 				placeholder='Type to search...'
 			></input>
 		</div>
