c343dc94b62e4f593e13424979ec62979f75f7b7
Himanshu Notani <hnotani7@gmail.com>
2022-02-25 11:03:32 +0530

    Update SearchBox.js

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,15 +1,17 @@
 import React from 'react';
+// import useRef from 'react';
+// import debounce from 'lodash.debounce';
 
 const SearchBox = (props) => {
-	const debouncedSave = useRef(debounce(nextVal => props.setSearchValue(nextVal), 1000))
-		.current;
+// 	const debouncedSave = useRef(debounce(nextVal => props.setSearchValue(nextVal), 1000))
+// 		.current;
 	
 	const handleSearch = (event) =>{
 
 		const nextVal = event.target.value;
-		//props.setSearchValue(nextVal);
+		props.setSearchValue(nextVal);
 		
-		debouncedSave(nextVal);
+		//debouncedSave(nextVal);
 	}
 	return (
 		<div className='col col-sm-4'>
